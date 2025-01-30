74aa0f7ce9650ea595278d09e1783eae94af2071
dhyanashri <dhyanashri.raman@gmail.com>
2025-01-29 21:16:26 -0500

    used file to store all names from json file

--- project1.py
+++ project1.py
@@ -3,4 +3,14 @@ import json
 with open("LinkedInProfiles.json", "r", encoding="utf-8") as file:
     data = json.load(file)  
 
-print(json.dumps(data[5], indent=4))
+with open("names.txt", "w", encoding="utf-8") as file:
+    for line in data:
+        name = line.get("name", None)  
+        if name is None:
+            continue
+        print(name)
+        file.write(name + "\n")
+        
+
+# with open("output.txt", "w", encoding="utf-8") as file:
+#     json.dump(random_entry, file, indent=4)
